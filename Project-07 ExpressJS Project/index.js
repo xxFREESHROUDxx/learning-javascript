1a5f98413aa15aa3571fad38c8cdbe57d898a684
xxFREESHROUDxx <baibhavkc11@gmail.com>
2023-03-02 17:08:23 +0545

    removed node_modules folder and project 07 done

---
+++ Project-07 ExpressJS Project/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const bicycles = require('./data/data.json');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  return res.render('bicycles', {
+    bicycles,
+  });
+});
+
+app.get('/bicycle', (req, res) => {
+  const bicycle = bicycles.find((b) => b.id === req.query.id);
+  return res.render('overview', {
+    bicycle,
+  });
+});
+
+app.listen(3000, () => console.log('Server is running at port 3000'));
